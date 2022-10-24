362da902a056fa35e0a38b6b16bde05718ce82bd
Marina Shalamova <tusya120@mail.ru>
2022-10-24 17:58:33 +0300

    second commit popups

--- element-search/popups/task.js
+++ element-search/popups/task.js
@@ -1,23 +1,25 @@
 const modalMain = document.getElementById("modal_main");
 const modalSuccess = document.getElementById("modal_success");
 const showSuccess = document.getElementsByClassName("show-success");
+console.log(showSuccess)
 let array = Array.from(document.getElementsByClassName("modal__close_times"));
 
 modalMain.classList.add("modal_active");
- 
+
 let informationButton1 = showSuccess.item(0);
 console.log(informationButton1);
 informationButton1.onclick = function () {
+    modalMain.classList.remove("modal_active");
     modalSuccess.classList.add("modal_active");
 };
 
-closed ();
+closed();
 
- function  closed () {
-array.forEach(function (element) {
-    element.onclick = function () {
-        modalMain.classList.remove("modal_active");
-        modalSuccess.classList.remove("modal_active");
-    };
-});
+function closed() {
+    array.forEach(function (element) {
+        element.onclick = function () {
+            modalMain.classList.remove("modal_active");
+            modalSuccess.classList.remove("modal_active");
+        };
+    });
 }
