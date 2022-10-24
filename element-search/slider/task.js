22c53cbbadc323550f24f8a94ad524dbba290953
Marina Shalamova <tusya120@mail.ru>
2022-10-24 18:05:54 +0300

    second commit slider

--- element-search/slider/task.js
+++ element-search/slider/task.js
@@ -1,66 +1,35 @@
 const slider = document.getElementsByClassName("slider__item");
-const arrayslider = Array.from(slider);
+const arraySlider = Array.from(slider);
 // console.log(arrayslider);
-const slider__arrow_next = document.querySelector(".slider__arrow_next");
-const slider__arrow_prev = document.querySelector(".slider__arrow_prev");
+const sliderNext = document.querySelector(".slider__arrow_next");
+const sliderPrev = document.querySelector(".slider__arrow_prev");
 
 let sliderNumber = 0;
-slider__arrow_next.onclick = function () {
-    if (arrayslider[sliderNumber].classList.contains("slider__item_active")) {
-        arrayslider[sliderNumber].classList.remove("slider__item_active");
-    };
 
-    arrayslider[sliderNumber + 1].classList.add("slider__item_active");
-    // console.log(arrayslider);
-
-    console.log(arrayslider.length);
-    if (sliderNumber < arrayslider.length - 1) {
-        sliderNumber = sliderNumber + 1;
-        // console.log(sliderNumber);
-    } else {
-        sliderNumber = 0;
-        arrayslider[sliderNumber].classList.add("slider__item_active");
-        console.log(arrayslider);
-    }
+sliderNext.onclick = function () {
+    sliderNumber++;
+    check();
+    document.getElementsByClassName("slider__item_active")[0].classList.remove("slider__item_active");
+    arraySlider[sliderNumber].classList.add("slider__item_active");
 };
 
-let sliderLastNumber = arrayslider.length;
-slider__arrow_prev.onclick = function () {
+sliderPrev.onclick = function () {
+    sliderNumber--;
+    check();
+    document.getElementsByClassName("slider__item_active")[0].classList.remove("slider__item_active");
+    arraySlider[sliderNumber].classList.add("slider__item_active");
+};
 
-    if (arrayslider[sliderNumber].classList.contains("slider__item_active")) {
-        arrayslider[sliderNumber].classList.remove("slider__item_active");
-        arrayslider[sliderNumber - 1].classList.add("slider__item_active");
+//  ПРОВЕРКА
+function check() {
+    if (sliderNumber >= arraySlider.length) {
+        sliderNumber = 0;
     };
-    // console.log(arrayslider);
-    // console.log(arrayslider.length);
-    if (sliderNumber > 0) {
-        sliderNumber = sliderNumber - 1;
-        // console.log(sliderNumber)
-    } else {
-        sliderNumber = arrayslider.length;
-        arrayslider[sliderNumber - 1].classList.add("slider__item_active");
-        arrayslider[sliderNumber].classList.remove("slider__item_active");
-        // console.log(arrayslider);
+
+    if (sliderNumber < 0) {
+        sliderNumber = arraySlider.length - 1;
+        console.log(sliderNumber);
+        arraySlider[sliderNumber].classList.add("slider__item_active");
     };
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+};
 
