bc386019d4165bfa0a9e13e09b3906c9a2d24a26
Marina Shalamova <tusya120@mail.ru>
2022-11-07 18:14:13 +0300

    first commit interests

---
+++ html-forms/interests/task.js
@@ -0,0 +1,34 @@
+const checkBoxes = [...document.querySelectorAll(".interest__check")];
+
+checkBoxes.forEach((el) => {
+    el.addEventListener("click", () => {
+
+        const closest = el.closest("label");
+
+        if (closest.nextElementSibling) {
+            const checkBox = [...closest.nextElementSibling.querySelectorAll(".interest__check")];
+
+            checkBox.forEach((i) => {
+                if (el.checked) {
+                    i.checked = true;
+                } else {
+                    i.checked = false;
+                };
+            });
+        } else {
+            el.checked = false;
+        };
+    });
+})
+
+
+
+
+
+
+
+
+
+
+
+
