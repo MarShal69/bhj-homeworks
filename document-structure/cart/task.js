fcef2d3835dd9891e036a50d1ae4ef118bbb7ff0
Marina Shalamova <tusya120@mail.ru>
2022-11-12 20:19:29 +0300

    first commit cart

---
+++ document-structure/cart/task.js
@@ -0,0 +1,45 @@
+const productQuantityControlDec = Array.from(document.querySelectorAll(".product__quantity-control_dec"));
+const productQuantityControlInc = Array.from(document.querySelectorAll(".product__quantity-control_inc"));
+const product = Array.from(document.querySelectorAll(".product"));
+const cartProducts = document.querySelector(".cart__products");
+const productAdd = document.querySelector(".product__add")
+
+productQuantityControlInc.forEach((el) => {
+    el.addEventListener("click", () => {
+        const productQuantityValue = el.closest(".product__quantity-controls").querySelector(".product__quantity-value");
+        let amount = + productQuantityValue.textContent;
+        amount += 1;
+        productQuantityValue.textContent = amount;
+    });
+});
+
+productQuantityControlDec.forEach((el) => {
+    el.addEventListener("click", () => {
+        const productQuantityValue = el.closest(".product__quantity-controls").querySelector(".product__quantity-value");
+        let amount = + productQuantityValue.textContent;
+        if (amount > 0) {
+            amount -= 1;
+            productQuantityValue.textContent = amount;
+        };
+    });
+});
+
+product.forEach((el) => {
+    const productAdd1 = el.querySelector(".product__add");
+
+    productAdd1.onclick = function () {
+        const productQuantityValue = el.querySelector(".product__quantity-value")
+        let amount = + productQuantityValue.textContent;
+
+        if (amount > 0) {
+            let iden = el.getAttribute("data-id");
+            let src = el.querySelector(".product__image").getAttribute("src");
+            cartProducts.insertAdjacentHTML("beforeend", `
+    <div class="cart__product" data-id = ${iden}>
+        <img class="cart__product-image" src=${src}>
+        <div class="cart__product-count"> ${el.querySelector(".product__quantity-value").textContent}</div>
+    </div>
+    `);
+        };
+    };
+});
