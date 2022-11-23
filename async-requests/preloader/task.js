ef0b855244cebe9c8bce0f92b3b79f213799cacd
Marina Shalamova <tusya120@mail.ru>
2022-11-23 19:11:31 +0300

    second commit preloader

--- async-requests/preloader/task.js
+++ async-requests/preloader/task.js
@@ -1,37 +1,33 @@
 const loader = document.querySelector(".loader");
 const items = document.getElementById("items");
 const item = document.querySelector(".item");
-
-
 const xhr = new XMLHttpRequest();
-// сщздаём объект класса
-xhr.addEventListener('readystatechange', () => {
 
-    if (xhr.readystate === xhr.DONE) {
-        // если запрос выполнен, то 
-        loader.classList.remove('loader_active');  // убираем активный класс у img
-        console.log(xhr.responseText); // на всякий случай смотрю в консоли, что загрузилось и ничего не вижу
-        let text = JSON.parse(this.responseText); // превращаем в объект JS
-        console.log(text);  // опять ничего нет
+xhr.onreadystatechange = function () {
 
+    if (xhr.readyState === 4) {
+        loader.classList.remove('loader_active');  // 
+        let text = JSON.parse(this.responseText);
+        console.log(text);
 
-        // обегаем ассщциативный массив  Valute и вставляем в HTML код с полученными из Valute данными
-        Valute.forEach((element, i) => {
-            items.insertAdjacentElement("afterbegin", `
-            <div class="item__code">
-                            ${element[i].CharCode}
-                        </div>
-                        <div class="item__value">
-                        ${element[i].Value}
-                        </div>
-                        <div class="item__currency">
-                            руб.
-                        </div>
+        for (const i in text.response.Valute) {
+            items.insertAdjacentHTML("afterbegin", `
+            <div class="item">
+                <div class="item__code">
+                    ${text.response.Valute[i].CharCode}
+                </div>
+                <div class="item__value">
+                    ${text.response.Valute[i].Value}
+                </div>
+                <div class="item__currency">
+                    руб.
+                </div>
+                </div>
             `
             );
-        });
+        };
     };
-});
+};
 
-xhr.open('GET', 'https://netology-slow-rest.herokuapp.com');
+xhr.open('GET', 'https://netology-slow-rest.herokuapp.com/');
 xhr.send();
